7fba84a83e59d82630080fb9be5858d8094fcee1
0xk1f0 <fteppan@protonmail.com>
2023-09-22 11:45:14 +0200

    bump dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-
 import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: {
+        entrypoint: "./src/components/void.ts",
+    }
+  },
   integrations: [svelte(), tailwind()],
+  publicDir: "./src/public",
   output: "server",
   adapter: deno()
 });
