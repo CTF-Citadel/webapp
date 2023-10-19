c9a54cd19036823f04c30005030acda187734776
0xk1f0 <fteppan@protonmail.com>
2023-10-19 22:03:47 +0200

    add initial users page and adjust email template slightly

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
     import { DarkMode } from 'flowbite-svelte';
     import ArrowLeftToBracketOutline from 'flowbite-svelte-icons/ArrowLeftToBracketOutline.svelte';
+    import UserCircleOutline from 'flowbite-svelte-icons/UserCircleOutline.svelte';
 
     // from parent
     export let username = '';
+    export let interactive = false;
 
     // header text
     let headerText = 'CTF Citadel';
@@ -32,12 +34,21 @@
                 <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{headerText}</span>
             </a>
             <div class="flex items-center lg:order-2">
+                {#if interactive}
+                    <a href="/user">
+                        <button class="p-2.5">
+                            <UserCircleOutline />
+                        </button>
+                    </a>
+                {/if}
                 <DarkMode class="text-2xl hover:bg-surface-900 border-surface-900 focus:ring-surface-900" />
-                <form method="post" action="/logout">
-                    <button type="submit" class="p-2.5">
-                        <ArrowLeftToBracketOutline />
-                    </button>
-                </form>
+                {#if interactive}
+                    <form method="post" action="/logout">
+                        <button type="submit" class="p-2.5">
+                            <ArrowLeftToBracketOutline />
+                        </button>
+                    </form>
+                {/if}
             </div>
             <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                 <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
--- src/lib/lucia-email.ts
+++ src/lib/lucia-email.ts
@@ -4,6 +4,7 @@ import nodemailer from "nodemailer";
 const EMAIL_SRVC = process.env.EMAIL_SERVICE;
 const EMAIL_USER = process.env.EMAIL_NAME;
 const EMAIL_PASS = process.env.EMAIL_PASS;
+const INSTANCE_NAME = process.env.INSTANCE_NAME;
 
 let transporter = nodemailer.createTransport({
     service: EMAIL_SRVC,
@@ -24,11 +25,24 @@ export const sendVerificationLink = async (origin: string, email: string, token:
             <title>Verify Your Email Address</title>
         </head>
             <body>
-                <p>Thank you for creating an account on CTF-Citadel. To complete the registration process, please click the link below to verify your email address:</p>
-                <a href="${URL}" style="text-decoration: none; padding: 10px 20px; background-color: #007BFF; color: #fff; border-radius: 4px; display: inline-block;">Verify Email</a>
+                <table width="100%" cellspacing="0" cellpadding="0">
+                    <tr>
+                        <td align="center">
+                            <h1 style="text-align: center;">Verify Your Email</h1>
+                        </td>
+                    </tr>
+                </table>
+                <p>Thank you for creating an account on ${INSTANCE_NAME}. To complete the registration process, please click the link below to verify your email address:</p>
+                <table width="100%" cellspacing="0" cellpadding="0">
+                    <tr>
+                        <td align="center">
+                            <a href="${URL}" style="text-decoration: none; padding: 10px 20px; background-color: #007BFF; color: #fff; border-radius: 4px; display: inline-block;">Verify Email</a>
+                        </td>
+                    </tr>
+                </table>
                 <p>By verifying your email, you will gain full access to our services and stay updated with the latest updates and notifications.</p>
                 <p>If you did not sign up for an account, please disregard this email.</p>
-                <p>Sincerely, <br>The CTF-Citadel Admin Team</p>
+                <p>Sincerely, <br>The ${INSTANCE_NAME} Team</p>
             </body>
         </html>`
     }, (err: any) => {
@@ -49,10 +63,23 @@ export const sendPasswordResetLink = async (origin: string, email: string, token
             <title>Reset Your Password</title>
         </head>
             <body>
-                <p>You are receiving this email because you have requested to reset your password for your account on CTF-Citadel. To proceed with the password reset, please click the link below:</p>
-                <a href="${URL}" style="text-decoration: none; padding: 10px 20px; background-color: #007BFF; color: #fff; border-radius: 4px; display: inline-block;">Reset Password</a>
+                <table width="100%" cellspacing="0" cellpadding="0">
+                    <tr>
+                        <td align="center">
+                            <h1 style="text-align: center;">Reset Your Password</h1>
+                        </td>
+                    </tr>
+                </table>
+                <p>You are receiving this email because you have requested to reset your password for your account on ${INSTANCE_NAME}. To proceed with the password reset, please click the link below:</p>
+                <table width="100%" cellspacing="0" cellpadding="0">
+                    <tr>
+                        <td align="center">
+                            <a href="${URL}" style="text-decoration: none; padding: 10px 20px; background-color: #007BFF; color: #fff; border-radius: 4px; display: inline-block;">Reset Password</a>
+                        </td>
+                    </tr>
+                </table>
                 <p>If you did not request this password reset, please ignore this email. Your password will remain unchanged.</p>
-                <p>Sincerely, <br>The CTF-Citadel Admin Team</p>
+                <p>Sincerely, <br>The ${INSTANCE_NAME} Admin Team</p>
             </body>
         </html>`
     }, (err: any) => {
