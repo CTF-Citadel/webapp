0e501a6fa6ffc1c48e8708b2590811eddaed42e5
0xk1f0 <fteppan@protonmail.com>
2023-10-20 11:55:21 +0200

    fix some CSS

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -26,12 +26,12 @@
     }
 </script>
 
-<header class="sticky top-0 w-full z-30">
+<header class="sticky top-0 w-screen z-30 min-w-screen h-16">
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="/" class="flex items-center">
+            <a href="/" class="items-center flex">
                 <img src="/favicon.svg" class="mr-3 h-6 sm:h-9" alt="Logo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{headerText}</span>
+                <span class="hidden lg:block self-center text-xl font-semibold whitespace-nowrap dark:text-white">{headerText}</span>
             </a>
             <div class="flex items-center lg:order-2">
                 {#if interactive}
