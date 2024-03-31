87f06a0ffe9a59052b094bf1b194043712620a30
0xk1f0 <dev@k1f0.dev>
2024-03-31 20:38:19 +0200

    feat(info): add support/rules page

--- src/components/app/pages/Dashboard.svelte
+++ src/components/app/pages/Dashboard.svelte
@@ -13,10 +13,17 @@
         DASHBOARD
     </h1>
     <div class="flex flex-col lg:flex-row items-center justify-center flex-1">
+        <Card img="/img/banners/03.webp" class="text-center m-2 bg-[#0000001f] dark:bg-[#0000004f] border-2 border-neutral-200 dark:border-neutral-800 backdrop-blur-3xl">
+            <a href="/rules">
+                <Button>
+                    Rules <ArrowRightOutline class="w-5 h-5 ml-1 text-white" />
+                </Button>
+            </a>
+        </Card>
         <Card img="/img/banners/01.webp" class="text-center m-2 bg-[#0000001f] dark:bg-[#0000004f] border-2 border-neutral-200 dark:border-neutral-800 backdrop-blur-3xl">
             <a href="/teams">
                 <Button>
-                    Teams <ArrowRightOutline class="w-5 h-5 ml-1 text-white" />            
+                    Teams <ArrowRightOutline class="w-5 h-5 ml-1 text-white" />
                 </Button>
             </a>
         </Card>
