1798cee720417426aea2509d7dd8e0bdefb56587
0xk1f0 <dev@k1f0.dev>
2024-04-18 00:51:04 +0200

    fix(docker): adjust entrypoint script

--- misc/entrypoint.sh
+++ misc/entrypoint.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
+export NODE_ENV=production
+export HOST=0.0.0.0
+export PORT=8085
+
 echo """
-###################
-  ____ _____ ____  
- / ___|_   _|  _ \ 
-| |     | | | | | |
-| |___  | | | |_| |
- \____| |_| |____/ 
-
-###################
-
-[+] Starting ...
+              __
+             /  \
+            / /\ \
+           /_/  \_\
+  __   ____ _____ ____   __
+ / /  / ___|_   _|  _ \  \ \
+/ /  | |     | | | | | |  \ \
+\ \  | |___  | | | |_| |  / /
+ \_\  \____| |_| |____/  /_/
+           __    __
+           \ \  / /
+            \ \/ /
+             \__/
 """
 
-echo "[+] Trying to migrate DB Schema ..."
+echo "[+] Migrating DB Schema ..."
 
-while ! $(npm run migrate 1> /dev/null); do
-    echo "Migration Error!"
+while ! $(npx drizzle-kit push:pg 1> /dev/null); do
+    echo "[-] Migration Error!"
     sleep 5;
 done
 
 echo "[+] Done! Starting Web Application ..."
 
-npm run dist
+node ./dist/server/entry.mjs
