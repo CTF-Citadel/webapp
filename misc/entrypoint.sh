ca88998672c62982e861405b5530f50220dee45a
0xk1f0 <dev@k1f0.dev>
2024-04-18 01:05:51 +0200

    fix(docker): fix my inability to write bash

--- misc/entrypoint.sh
+++ misc/entrypoint.sh
@@ -22,7 +22,7 @@ echo '''
 
 echo "[+] Migrating DB Schema ..."
 
-while ! $(npx drizzle-kit push:pg 2>&1 /dev/null); do
+while ! $(npx drizzle-kit push:pg 1> /dev/null 2> /dev/null); do
     echo "[-] Migration Error! Retrying in 3 seconds ..."
     sleep 3;
 done
